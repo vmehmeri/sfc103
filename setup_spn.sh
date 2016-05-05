661e5572dd370a319d634c706e50e476fb329d96
Victor Mehmeri <vime@fotonik.dtu.dk>
2016-05-05 10:14:48 -0500

    Fixed setup_spn script

--- setup_spn.sh
+++ setup_spn.sh
@@ -4,3 +4,5 @@ apt-get update -y
 apt-get install git curl -y
 curl -fsSL https://get.docker.com/ | sh
 docker build -t spn /vagrant/spn 
+docker run -d --name von1 spn tail -f /dev/null
+docker run -d --name von2 spn tail -f /dev/null
